4f4b2265bb9e976a2ee5ed9f6596724ad5dca70f
Joelp48 <i2422400@continental.edu.pe>
2024-09-22 17:02:09 -0500

    Agregando Descripción

--- semana2.py
+++ semana2.py
@@ -2,3 +2,6 @@ import streamlit as st
 
 #titulo a la aplicacion
 st.title("Introduccion a variables y tipos de datos Python")
+
+#descripcion inicial
+st.write("Python es un lenguaje de programacion donde...")
