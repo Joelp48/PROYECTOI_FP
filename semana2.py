bed773b6f0e9a5e660e77757326cc7410b2b7803
Joelp48 <i2422400@continental.edu.pe>
2024-09-22 17:27:12 -0500

    definiendo valores enteros

--- semana2.py
+++ semana2.py
@@ -5,3 +5,11 @@ st.title("Introduccion a variables y tipos de datos Python")
 
 #descripcion inicial
 st.write("Python es un lenguaje de programacion donde...")
+
+#sección para varible de tipo entero
+st.header("Ejemplo 1: Enteros")
+st.write("En python un entero (integer) es un numero sin decimales por ejemplo:")
+
+#Definir una variable entera
+int_variable = 42
+st.code(f"int_variable = (int_variable) # Tipo: {type(int_variable)}")
