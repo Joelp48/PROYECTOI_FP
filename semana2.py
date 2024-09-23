7f994e07ed503f70fe4de1c5b36280fb9a559cab
Joelp48 <i2422400@continental.edu.pe>
2024-09-22 22:58:45 -0500

    ingresando datos con teclado

--- semana2.py
+++ semana2.py
@@ -10,6 +10,8 @@ st.write("Python es un lenguaje de programacion donde...")
 st.header("Ejemplo 1: Enteros")
 st.write("En python un entero (integer) es un numero sin decimales por ejemplo:")
 
-#Definir una variable entera
-int_variable = 42
+#Input para que el usuario ingrese un dato(de preferencia un numero entero)
+int_variable = st.number_input("ingrese un numero entero:", value=42, step=1)
+
+#Mostrando el valor
 st.code(f"int_variable = (int_variable) # Tipo: {type(int_variable)}")
